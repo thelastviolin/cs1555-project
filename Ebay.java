2d4baa39ba4493e3d674c7843931a9aea03089f5
Robert <boatingpanda@users.noreply.github.com>
2015-04-15 16:55:32 -0400

    fixed login handler query

--- Ebay.java
+++ Ebay.java
@@ -13,8 +13,8 @@ public class Ebay {
 	public static void main(String[] args) {
 		boolean timeToQuit;
 		String username, password;
-		username = "kdi8"; //This is your username in oracle
-		password = "3722875"; //This is your password in oracle
+		username = "zhw28"; //This is your username in oracle
+		password = "3665394"; //This is your password in oracle
 
 		try {
 			// Register the oracle driver.  
@@ -30,7 +30,7 @@ public class Ebay {
 
 			String login = auctionHouse.handleLogin();
 			while (!auctionHouse.timeToQuit) {
-
+				
 			}
 			System.out.println("\nGoodbye!");
 
@@ -62,9 +62,9 @@ public class Ebay {
 			if (option == 1) {
 				String username, password;
 				System.out.print("\nUsername: ");
-				username = scan.nextLine();
-				System.out.print("\nPassword: ");
-				password = scan.nextLine();
+				username = scan.next();
+				System.out.print("Password: ");
+				password = scan.next();
 
 				PreparedStatement getAdmin = null;
 				PreparedStatement getCustomer = null;
@@ -72,8 +72,8 @@ public class Ebay {
 				try {
 					ResultSet rs;
 					isAdmin = false;
-					String queryAdmin = "select count(*) from administrator where username = ? and password = ?";
-					String queryCustomer = "select count(*) from customer where username = ? and password = ?";
+					String queryAdmin = "select count(*) from administrator where login = ? and password = ?";
+					String queryCustomer = "select count(*) from customer where login = ? and password = ?";
 					getAdmin = connection.prepareStatement(queryAdmin);
 					getCustomer = connection.prepareStatement(queryCustomer);
 					getAdmin.setString(1, username);
@@ -119,6 +119,45 @@ public class Ebay {
 		}
 		this.adminLogin = isAdmin;
 		return login;
+	}
+	
+	public void updateSysDate(String newDate){
+		
+		try{
+			
+			java.text.SimpleDateFormat df = new java.text.SimpleDateFormat("DD-MON-YYYY/HH:MI:SSAM");
+			java.sql.Date new_date = new java.sql.Date (df.parse(newDate).getTime());
+			
+			Statement statement = connection.createStatement();
+			String compareSysDateQuery = "SELECT c_date FROM ourSysDate";
+			ResultSet result = statement.executeQuery(compareSysDateQuery);
+			int updateResult = 1;
+			
+			while(result.next()){
+				
+				System.out.println(result.getDate(1));
+				// if(result.getDate(1).getTime() == new_date){
+				// 	System.out.println("New time is the same as old time, you're crazy");
+				// }
+				// else if(result.getDate(1).getTime() > new_date){
+				// 	System.out.println("System Date cannot be set back into the past.");
+				// }
+				// else{
+				// 	System.out.println("System time is smaller than new time, update system time");
+				// 	String updateSysDateQuery = "update ourSysDate set c_date = " + new_date;
+				// 	updateResult = statement.executeUpdate(updateSysDateQuery);
+				// }
+				
+			}
+			
+		}
+		catch (SQLException e) {
+			System.out.println("Error running queries. Machine Error: " + e.toString());
+		}
+		catch (ParseException e) {
+			System.out.println("Error parsing data. Machine error: " + e.toString());
+		}
+		
 	}
 
 }
